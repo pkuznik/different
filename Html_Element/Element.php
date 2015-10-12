35984a114fbb21337a48b3f71cc419236adbbff9
Piotr Kuźnik <piotr.damian.kuznik@gmail.com>
2015-10-12 14:49:03 +0200

    Create Element.php

---
+++ Html_Element/Element.php
@@ -0,0 +1,27 @@
+/**
+ * Description of Element
+ * @version 0.1
+ * @author Piotr Kuźnik <piotr.damian.kuznik@gmail.com>
+ * @license https://www.gnu.org/copyleft/gpl.html GNU
+ * @copyright
+ */
+ 
+ abstract class Element {
+    
+    proctected $name = null;
+    
+    /*
+     * @param $name Identfikator html 
+    public function __construct($name){
+        if ( is_string($name) == false || empty($name) ){
+            trigger_error( "In construct " . __CLASS__ . "  you must set $name and his cannot be empty." );
+        }    
+        $this->name = $name;
+    }
+    
+    /**
+     * Zwraca html danego elementu
+    public function toHtml(){
+        return '';
+    }
+ }
